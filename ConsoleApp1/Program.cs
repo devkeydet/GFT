a00b9599a36c16175e5cbe481f5a0119862fcefb
devkeydet <devkeydet@gmail.com>
2017-04-18 08:48:40 -0400

    Init

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,12 @@
         static void Main()
         {
             SomeFeature();
+            AnotherFeature();
+        }
+
+        private static void AnotherFeature()
+        {
+            throw new System.NotImplementedException();
         }
 
         private static void SomeFeature()
