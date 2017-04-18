9fb6c023b25661d31e90d268c9698ae0df758190
devkeydet <devkeydet@gmail.com>
2017-04-18 08:08:13 -0400

    Cleanup

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
         }
     }
